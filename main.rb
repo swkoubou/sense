8bb98bba95be02b2df4f1b9381c27e7abc44a616
Kazuya Igari <twgiko@gmail.com>
2016-08-06 17:23:54 +0900

    :sparkles: DB生成処理の実装

--- main.rb
+++ main.rb
@@ -2,6 +2,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
 
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/db.sqlite3"
+)
+
 get '/' do
   "test"
 end
