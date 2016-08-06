2e80d15d0a4eb7ab96ee8b0bf9cb9c4a65a1cb20
Kazuya Igari <twgiko@gmail.com>
2016-08-06 19:02:10 +0900

    :new: :sparkles: Quizモデルの追加とmainから使えるように修正

--- main.rb
+++ main.rb
@@ -2,12 +2,14 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
 
+require_relative "models/quiz"
+
 ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "db/db.sqlite3"
 )
 
 get '/' do
-  "test"
+  Quiz.count.to_s
 end
 
---
+++ models/quiz.rb
@@ -0,0 +1,4 @@
+require "active_record"
+
+class Quiz < ActiveRecord::Base
+end
