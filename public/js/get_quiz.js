68c520546ff3e3ce47525746914e623679823353
Kazuya Igari <twgiko@gmail.com>
2016-08-06 22:21:34 +0900

    :new: 問題取得処理を実装

---
+++ public/js/get_quiz.js
@@ -0,0 +1,12 @@
+function get_quiz(){
+    $.ajax({
+        type: 'get',
+        url: '/quiz',
+    }).done(function(res)){
+        console.log("OK.");
+        console.log(res);
+    }).fail({function(err)){
+        console.log("err.");
+        console.log(err);
+    };
+}
